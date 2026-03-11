8d29b95876ad371d8c05bea0bc10dd8260a9fac5
udhayanagaraj <udhayanagaraj7102001@gmail.com>
2026-03-11 22:19:26 +0530

    Modules class

---
+++ Management/main.py
@@ -0,0 +1,4 @@
+from .teacher import is_valid_aadhar
+
+
+print(is_valid_aadhar("1234123412"))
---
+++ Management/teacher.py
@@ -0,0 +1,9 @@
+
+
+def is_valid_aadhar(aadhar_no):
+    n = len(aadhar_no)
+
+    if n != 12:
+        return "Invalid Aadhar number"
+    
+    return "Valid Aadhar number"
